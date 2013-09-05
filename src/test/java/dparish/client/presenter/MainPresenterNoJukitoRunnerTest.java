366b6f5210b48503c4c33af684fe6daf321a7d10
David Parish <dparish@gmail.com>
2013-09-05 13:49:21 -0500

    Testing mock messages

--- src/test/java/dparish/client/presenter/MainPresenterNoJukitoRunnerTest.java
+++ src/test/java/dparish/client/presenter/MainPresenterNoJukitoRunnerTest.java
@@ -1,11 +1,17 @@
 package dparish.client.presenter;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.GWTMockUtilities;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import org.jukito.SandboxTestModule;
+import dparish.client.resources.MainMessages;
+import dparish.test.inject.SandboxTestModule;
 import dparish.client.view.MainView;
 import dparish.client.view.Page;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * @author dparish
@@ -13,6 +19,23 @@ import org.junit.Test;
 public class MainPresenterNoJukitoRunnerTest {
 
     Injector injector = Guice.createInjector(new SandboxTestModule());
+
+    @Before
+    public void gwtNoBrowser() {
+        GWTMockUtilities.disarm();
+        GWTMockUtilities.returnMockMessages();
+    }
+
+    @After
+    public void gwtReset() {
+        GWTMockUtilities.restore();
+    }
+
+    @Test
+    public void testMessages() {
+        MainMessages messages = GWT.create(MainMessages.class);
+        assertNotNull(messages);
+    }
 
     @Test
     public void testMain() {
--- src/test/java/dparish/test/inject/SandboxTestModule.java
+++ src/test/java/dparish/test/inject/SandboxTestModule.java
@@ -1,10 +1,15 @@
-package org.jukito;
+package dparish.test.inject;
+
+import java.lang.reflect.Constructor;
 
 import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import dparish.client.presenter.MainPresenter;
 import dparish.client.view.MainView;
+import org.jukito.BindingsCollector;
+import org.jukito.JukitoModule;
 
 /**
  * @author dparish
@@ -12,9 +17,6 @@ import dparish.client.view.MainView;
  * This module is my test at abstracting out the Jukito initializations so that I could run my tests
  * without a JukitoRunner.
  *
- * I had to put this in the org.jukito package because the Constructor for BindingsCollector is
- * package protected.
-
  */
 public class SandboxTestModule extends JukitoModule {
     @Override
@@ -33,7 +35,19 @@ public class SandboxTestModule extends JukitoModule {
 
     // This is mimicking what JukitoTestRunner does so we can test w/o the runner.
     public SandboxTestModule() {
-        BindingsCollector collector = new BindingsCollector(this);
+        BindingsCollector collector = null;
+        try {
+            // All this is because the constructor for BindingsCollector is packaged protected.
+            Class collectorClass = Class.forName("org.jukito.BindingsCollector");
+            Class[] args = new Class[1];
+            args[0] = AbstractModule.class;
+            Constructor constructor = collectorClass.getDeclaredConstructor(args);
+            constructor.setAccessible(true);
+            collector = (BindingsCollector) constructor.newInstance(this);
+
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
         collector.collectBindings();
         setBindingsObserved(collector.getBindingsObserved());
     }
